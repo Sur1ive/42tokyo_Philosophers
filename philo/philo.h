e0028f29db02a86186f04a4fa0f962b71f53d529
yxu <penguinxx2@gmail.com>
2024-05-11 16:46:34 +0900

    init project

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yxu <yxu@student.42tokyo.jp>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 15:39:58 by yxu               #+#    #+#             */
+/*   Updated: 2024/05/11 16:42:24 by yxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# define MALLOC_ERROR 1
+# define NUM_OF_ARGUMENTS_ERROR 2
+# define FORMAT_OF_ARGUMENTS_ERROR 3
+# define DUPLICATES_IN_ARGUMENTS 4
+# define TOO_MUCH_ELEMENTS 5
+# define NO_SUCH_CMD 6
+
+#endif
